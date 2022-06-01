40afd1cdf3f4a2af77b59e82750ba68d95f0f435
qiaoshiyang <1391343707@qq.com>
2022-06-01 19:07:40 +0800

    【create】项目创建；

---
+++ src/main/java/com/qsy/demo/ProjectTestApplication.java
@@ -0,0 +1,18 @@
+package com.qsy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ProjectTestApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(ProjectTestApplication.class, args);
+		System.out.println("项目启动，启动环境：" + Arrays.toString(context.getEnvironment().getActiveProfiles()));
+	}
+
+}
---
+++ src/test/java/com/qsy/demo/ProjectTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
