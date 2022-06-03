7524b0f0c8d5e5dc5167d4c3b0c53b8a74e65c38
qsy <1391343707@qq.com>
2022-06-03 08:34:40 +0800

    【update】添加日志打印功能；

--- src/main/java/com/qsy/demo/ProjectTestApplication.java
+++ src/main/java/com/qsy/demo/ProjectTestApplication.java
@@ -1,5 +1,6 @@
 package com.qsy.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,11 +9,12 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.util.Arrays;
 
 @SpringBootApplication
+@Slf4j
 public class ProjectTestApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(ProjectTestApplication.class, args);
-		System.out.println("测试项目启动，启动环境：" + Arrays.toString(context.getEnvironment().getActiveProfiles()));
+		log.info("项目启动，启动环境：【{}】", Arrays.toString(context.getEnvironment().getActiveProfiles()));
 	}
 
 }
